b1b9fb71114d174f141d083e61788a79287cbbfd
theDe-bugger <jaimil.d@icloud.com>
2023-09-26 17:42:08 -0400

    updated to fix build

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,37 +1,15 @@
 import IconButton from '@mui/material/IconButton';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Stack from '@mui/material/Stack';
 import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import Typed from "typed.js";
-import { useEffect, useRef } from "react";
 
 function Hero() {
-    // const el = useRef(null);
-
-    // useEffect(() => {
-    //     const typed = new Typed(el.current, {
-    //     strings: ["CS @ University of Waterloo", "Computer Vision + Back-End @ Martinrea", "Software Engineer Intern @ Finastra", "Co-Founder, Director of Tech @ Wat Street"], // Strings to display
-    //     // Speed settings, try diffrent values untill you get good results
-    //     startDelay: 300,
-    //     typeSpeed: 50,
-    //     backSpeed: 50,
-    //     backDelay: 2000,
-    //     loop: true
-    //     });
-
-    //     // Destropying
-    //     return () => {
-    //     typed.destroy();
-    //     };
-    // }, []);
 
     return ( 
         <div className='App-header'>
         <main>
             <h1>
-                <></>
                 Wat Street
             </h1>
             <h4>
