0797a60a7f92c649452ff4eeefc47473a2b33093
theDe-bugger <jaimil.d@icloud.com>
2023-11-30 23:41:00 -0500

    sponsors section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from "react";
 import Hero from "./components/Hero";
+import Sponsors from "./components/Sponsors";
 function App() {
   return (
     <div>
       <Hero />
+      <Sponsors />
     </div>
     
   );
