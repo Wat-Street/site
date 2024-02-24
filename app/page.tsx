45441d449e8162d0595dd7080790a50eb2effa94
Ishaan Dey <ishaan1013@gmail.com>
2024-02-23 22:03:15 -0500

    complete change

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen w-screen">
+      <h1>Wat Street</h1>
+      <div>The University of Waterloo's quantitative finance design team.</div>
+    </main>
+  )
+}
