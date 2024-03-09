8c875a1a7c7083c97f3c72832180349887b8d241
Ishaan Dey <ishaan1013@gmail.com>
2024-03-09 16:34:53 -0500

    Change font to Proxima Nova

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,35 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import localFont from "next/font/local"
 import "./globals.css"
 import { ThemeProvider } from "@/components/layout/themeProvider"
 
-const inter = Inter({ subsets: ["latin"] })
+// const inter = Inter({ subsets: ["latin"] })
+
+const proxima = localFont({
+  src: [
+    // {
+    //   path: "./fonts/proxima_nova_light.woff2",
+    //   weight: "400",
+    //   style: "normal",
+    // },
+    {
+      path: "./fonts/proxima_nova_regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/proxima_nova_semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/proxima_nova_bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: "Wat Street",
@@ -17,7 +43,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={proxima.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
