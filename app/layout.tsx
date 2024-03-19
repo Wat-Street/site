041185e99f541977a815fec08770929f6fb302c0
Akshar Barot <akshar.barot67@gmail.com>
2024-03-18 20:46:36 -0400

    base layout + navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,29 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/layout/themeProvider"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/ui/Navbar";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wat Street",
-  description: "The University of Waterloo's quantitative finance design team.",
-}
+    title: "Wat Street",
+    description:
+        "The University of Waterloo's quantitative finance design team.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          forcedTheme="dark"
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} bg-black text-white antialiased selection:bg-[#FFBB0F] selection:text-yellow-950 max-w-screen`}
+            >
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,16 @@
-import Image from "next/image"
-
-import Logo from "@/assets/images/watstreet.png"
-import { SiLinkedin, SiInstagram } from "@icons-pack/react-simple-icons"
-import { Mail } from "lucide-react"
+import Image from "next/image";
 
 export default function HomePage() {
-  return (
-    <main className="flex justify-center min-h-screen w-screen px-4">
-      <div className="w-full flex flex-col items-center max-w-screen-lg">
-        <nav className="w-full py-4 mb-32 flex justify-between items-center">
-          <Image src={Logo} alt="Wat Street logo" className="w-16 h-16" />
-          <div className="flex space-x-6 items-center">
-            <a
-              href=""
-              target="_blank"
-              className="duration-200 hover:text-muted-foreground"
-            >
-              <Mail className="w-7 h-7" />
-            </a>
-            <a
-              href=""
-              target="_blank"
-              className="duration-200 hover:text-muted-foreground"
-            >
-              <SiLinkedin className="w-6 h-6" />
-            </a>
-            <a
-              href=""
-              target="_blank"
-              className="duration-200 hover:text-muted-foreground"
-            >
-              <SiInstagram className="w-6 h-6" />
-            </a>
-          </div>
-        </nav>
-        <h1 className="font-semibold text-6xl text-brand">Wat Street</h1>
-        <div className="text-lg mt-4">
-          Waterloo's quantitative finance design team
-        </div>
-      </div>
-    </main>
-  )
+    return (
+        <main className="mx-auto min-h-screen max-w-screen-lg">
+            <div className="grid grid-rows-2 text-center pt-24">
+                <h1 className="text-6xl font-black tracking-widest">
+                    WAT STREET
+                </h1>
+                <p className="pt-6 text-xl tracking-widest font-light">
+                    UWaterloo's Quantitative Finance Team
+                </p>
+            </div>
+        </main>
+    );
 }
