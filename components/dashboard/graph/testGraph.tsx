b9824eba9274493f1570c8f6d37f877246c7c562
Ishaan Dey <ishaan1013@gmail.com>
2024-03-22 00:41:30 -0400

    x & y lines on graph

--- components/dashboard/graph/testGraph.tsx
+++ components/dashboard/graph/testGraph.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react"
-import { Group } from "@visx/group"
+import React, { useCallback, useState } from "react"
+import { Bar, Line } from "@visx/shape"
 import { BoxPlot } from "@visx/stats"
-import { scaleTime, scaleBand, scaleLinear } from "@visx/scale"
+import { scaleBand, scaleLinear } from "@visx/scale"
 import { AxisBottom, AxisRight } from "@visx/axis"
 import { TBoxPlot } from "./types"
+import { useTooltip, defaultStyles } from "@visx/tooltip"
+import { localPoint } from "@visx/event"
+import { max as visxMax, extent, bisector } from "@visx/vendor/d3-array"
 
 // // seeded randomness
 // const seededRandom = getSeededRandom(0.1)
@@ -12,7 +15,7 @@ import { TBoxPlot } from "./types"
 
 // accessors
 // const x = (d: TBoxPlot) => parseInt(d.x)
-const x = (d: TBoxPlot) => new Date(parseInt(d.x))
+const x = (d: TBoxPlot) => getDate(d)
 const min = (d: TBoxPlot) => d.min
 const max = (d: TBoxPlot) => d.max
 const median = (d: TBoxPlot) => d.median
@@ -20,13 +23,16 @@ const firstQuartile = (d: TBoxPlot) => d.firstQuartile
 const thirdQuartile = (d: TBoxPlot) => d.thirdQuartile
 
 interface TooltipData {
-  name: string
+  x: string
   min: number
   median: number
   max: number
   firstQuartile: number
   thirdQuartile: number
 }
+
+const getDate = (d: TBoxPlot) => new Date(parseInt(d.x))
+const bisectDate = bisector<TooltipData, Date>((d) => getDate(d)).left
 
 export default function TestGraph({
   width,
@@ -37,18 +43,21 @@ export default function TestGraph({
   height: number
   data: TBoxPlot[]
 }) {
-  const [tooltipData, setTooltipData] = useState<TooltipData | null>(null)
-
-  // const xScale = scaleTime<number>({
-  //   range: [0, width - 48],
-  //   domain: [parseInt(data[0].x), parseInt(data[data.length - 1].x)],
-  // })
+  // const [tooltipData, setTooltipData] = useState<TooltipData | null>(null)
+  const {
+    tooltipOpen,
+    tooltipLeft,
+    tooltipTop,
+    tooltipData,
+    hideTooltip,
+    showTooltip,
+  } = useTooltip<TooltipData>()
 
   const xScale = scaleBand<number>({
     range: [0, width - 48],
-    round: true,
+    // round: true,
     domain: data.map((d) => x(d).getTime()),
-    padding: 0.4,
+    paddingOuter: 2,
   })
 
   const values = data.reduce((allValues, boxPlot) => {
@@ -65,9 +74,43 @@ export default function TestGraph({
     nice: 100,
   })
 
-  const boxWidth = width / data.length
-  // const constrainedWidth = Math.min(40, boxWidth)
-  const constrainedWidth = boxWidth
+  const boxWidth = xScale.bandwidth()
+  const constrainedWidth = Math.min(40, boxWidth)
+
+  const handleTooltip = useCallback(
+    (
+      event: React.TouchEvent<SVGRectElement> | React.MouseEvent<SVGRectElement>
+    ) => {
+      const { x } = localPoint(event) || { x: 0 }
+      const offsetX = Math.max(
+        0,
+        x - xScale.paddingOuter() * xScale.bandwidth()
+      )
+
+      const eachBand = xScale.step()
+      const xIndex = Math.floor(offsetX / eachBand)
+      const x0 = xScale.domain()[xIndex]
+      const index = bisectDate(data, new Date(x0), 1)
+      const d0 = data[index - 1]
+      const d1 = data[index]
+      let d = d0
+      if (d1 && getDate(d1)) {
+        d =
+          x0.valueOf() - getDate(d0).valueOf() >
+          getDate(d1).valueOf() - x0.valueOf()
+            ? d1
+            : d0
+      }
+      showTooltip({
+        tooltipData: d,
+        tooltipLeft: x,
+        tooltipTop: yScale(
+          d.firstQuartile == d.median ? d.thirdQuartile : d.firstQuartile
+        ),
+      })
+    },
+    [showTooltip, yScale, xScale]
+  )
 
   return (
     <div className="relative h-full cursor-crosshair">
@@ -86,7 +129,6 @@ export default function TestGraph({
               min={min(d)}
               max={max(d)}
               left={xScale(x(d).getTime())! + 0.3 * constrainedWidth}
-              // left={xScale(x(d))! + 0.3 * constrainedWidth}
               firstQuartile={firstQuartile(d)}
               thirdQuartile={thirdQuartile(d)}
               median={median(d)}
@@ -96,61 +138,66 @@ export default function TestGraph({
               stroke={firstQuartile(d) === median(d) ? "#26ab6d" : "#d93637"}
               strokeWidth={1}
               valueScale={yScale}
-              minProps={{
-                onMouseOver: () => {
-                  setTooltipData({
-                    min: min(d),
-                    name: new Date(x(d)).toLocaleTimeString(),
-                    // name: x(d),
-                    max: max(d),
-                    firstQuartile: firstQuartile(d),
-                    median: median(d),
-                    thirdQuartile: thirdQuartile(d),
-                  })
-                },
-              }}
-              maxProps={{
-                onMouseOver: () => {
-                  setTooltipData({
-                    min: min(d),
-                    name: new Date(x(d)).toLocaleTimeString(),
-                    // name: x(d),
-                    max: max(d),
-                    firstQuartile: firstQuartile(d),
-                    median: median(d),
-                    thirdQuartile: thirdQuartile(d),
-                  })
-                },
-              }}
-              boxProps={{
-                onMouseOver: () => {
-                  setTooltipData({
-                    min: min(d),
-                    name: new Date(x(d)).toLocaleTimeString(),
-                    // name: x(d),
-                    max: max(d),
-                    firstQuartile: firstQuartile(d),
-                    median: median(d),
-                    thirdQuartile: thirdQuartile(d),
-                  })
-                },
-              }}
-              medianProps={{
-                style: {
-                  stroke: "white",
-                },
-                onMouseOver: () => {
-                  setTooltipData({
-                    min: min(d),
-                    name: new Date(x(d)).toLocaleTimeString(),
-                    // name: x(d),
-                    max: max(d),
-                    firstQuartile: firstQuartile(d),
-                    median: median(d),
-                    thirdQuartile: thirdQuartile(d),
-                  })
-                },
-              }}
+              // minProps={{
+              //   onMouseOver: () => {
+              //     setTooltipData({
+              //       min: min(d),
+              //       name: new Date(x(d)).toLocaleTimeString(),
+              //       max: max(d),
+              //       firstQuartile: firstQuartile(d),
+              //       median: median(d),
+              //       thirdQuartile: thirdQuartile(d),
+              //     })
+              //   },
+              // }}
+              // maxProps={{
+              //   onMouseOver: () => {
+              //     setTooltipData({
+              //       min: min(d),
+              //       name: new Date(x(d)).toLocaleTimeString(),
+              //       max: max(d),
+              //       firstQuartile: firstQuartile(d),
+              //       median: median(d),
+              //       thirdQuartile: thirdQuartile(d),
+              //     })
+              //   },
+              // }}
+              // boxProps={{
+              //   onMouseOver: () => {
+              //     setTooltipData({
+              //       min: min(d),
+              //       name: new Date(x(d)).toLocaleTimeString(),
+              //       max: max(d),
+              //       firstQuartile: firstQuartile(d),
+              //       median: median(d),
+              //       thirdQuartile: thirdQuartile(d),
+              //     })
+              //   },
+              // }}
+              // medianProps={{
+              //   onMouseOver: () => {
+              //     setTooltipData({
+              //       min: min(d),
+              //       name: new Date(x(d)).toLocaleTimeString(),
+              //       max: max(d),
+              //       firstQuartile: firstQuartile(d),
+              //       median: median(d),
+              //       thirdQuartile: thirdQuartile(d),
+              //     })
+              //   },
+              // }}
+            />
+            <Bar
+              x={0}
+              y={0}
+              width={innerWidth}
+              height={innerHeight}
+              fill="transparent"
+              rx={14}
+              onTouchStart={handleTooltip}
+              onTouchMove={handleTooltip}
+              onMouseMove={handleTooltip}
+              onMouseLeave={() => hideTooltip()}
             />
           </g>
         ))}
@@ -185,10 +232,34 @@ export default function TestGraph({
             dx: 16,
           }}
         />
+        {tooltipData && (
+          <g>
+            <Line
+              from={{ x: 0, y: tooltipTop }}
+              to={{ y: tooltipTop, x: innerWidth - 164 }}
+              stroke={"#fff"}
+              strokeOpacity={0.25}
+              strokeWidth={1}
+              pointerEvents="none"
+              strokeDasharray="5,2"
+            />
+            <Line
+              from={{ x: tooltipLeft, y: 0 }}
+              to={{ x: tooltipLeft, y: innerHeight - 164 }}
+              stroke={"#fff"}
+              strokeOpacity={0.25}
+              strokeWidth={1}
+              pointerEvents="none"
+              strokeDasharray="5,2"
+            />
+          </g>
+        )}
       </svg>
       {tooltipData && (
         <div className="p-2 top-2 left-2 absolute text-white rounded-lg shadow-sm border bg-neutral-950">
-          <div className="text-xs font-semibold">{tooltipData.name}</div>
+          <div className="text-xs font-semibold">
+            {new Date(parseInt(tooltipData.x)).toLocaleTimeString()}
+          </div>
           <div className="text-xs text-muted-foreground">
             {tooltipData.median && <div>Open: {tooltipData.median}</div>}
             {tooltipData.max && <div>High: {tooltipData.max}</div>}
