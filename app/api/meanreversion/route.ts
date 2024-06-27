d7c7051132383f613b8ee33b763810eeaeb280ed
Ishaan Dey <69771365+ishaan1013@users.noreply.github.com>
2024-06-27 00:00:05 -0700

    add request

---
+++ app/api/meanreversion/route.ts
@@ -0,0 +1,52 @@
+import { z } from "zod";
+
+const schema = z.object({
+  ticker: z.string(),
+  trainstart: z.string(),
+  trainend: z.string(),
+  teststart: z.string(),
+  testend: z.string(),
+  maxholding: z.string(),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  let parsed;
+
+  try {
+    parsed = schema.parse(body);
+  } catch (error) {
+    return Response.json({ error: "Invalid request" }, { status: 400 });
+  }
+
+  try {
+    const res = await fetch(
+      `https://immense-alert-osprey.ngrok-free.app/api/meanreversion`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(parsed),
+      }
+    );
+    const data = await res.json();
+
+    // const data = [
+    //   {
+    //     confidence: 0.9731735587120056,
+    //     datetime: "2022-12-06 00:00:00",
+    //     suggestion: "Buy",
+    //   },
+    //   {
+    //     confidence: 0.9925611019134521,
+    //     datetime: "2022-12-07 00:00:00",
+    //     suggestion: "Buy",
+    //   },
+    // ];
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error: "Failed to fetch data" }, { status: 500 });
+  }
+}
